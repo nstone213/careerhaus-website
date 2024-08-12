87e0f86475aa837e673ee4185dbc894bb5ce9a23
nstone213 <nbaronstone@gmail.com>
2024-08-12 00:51:17 -0400

    Changes to Card component: spacing and href

--- react-portfolio/src/components/AlumniCard/Card.jsx
+++ react-portfolio/src/components/AlumniCard/Card.jsx
@@ -12,15 +12,12 @@ export const Card = () => {
             <div className="card-total">
                 <div className="card-container">
                     <img src={Nicholas_Stone} alt="Card Image" className="card-image" draggable="false"/>
-                    <h1 className="card-title">Nicholas Stone</h1>
+                    <h1 className="card-title">
+                        <a href="https://www.linkedin.com/in/sidbalaga/" target="_blank" rel="noreferrer">
+                            Nicholas Stone
+                        </a>
+                    </h1>
                     <p className="card-description">Georgia Institute of Technology</p>
-                    <ul className="card-button">
-                        <li>
-                            <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/sidbalaga/'>
-                                <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-                            </a>
-                        </li>
-                    </ul>
                 </div>
             </div>
             <Loader type="line-scale-pulse-out"/>
