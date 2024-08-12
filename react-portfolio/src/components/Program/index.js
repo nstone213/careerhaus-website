59f1c67b4274ab0f28fdc4ebfcc4cfba4dbeaea4
nstone213 <nbaronstone@gmail.com>
2024-08-12 03:56:06 -0400

    Deleted unnecessary imports

--- react-portfolio/src/components/Program/index.js
+++ react-portfolio/src/components/Program/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import './index.scss';
-import { Card } from '../AlumniCard/Card';
 import Loader from 'react-loaders'
 import { CardRow } from '../AlumniCard/CardRow';
-import Nicholas_Stone from '../AlumniCard/AlumniPhotos/Nicholas_Stone_image.jpeg';
-import Aabha_Muley from '../AlumniCard/AlumniPhotos/AabhaMuley.png';
-import Ravnoor_Gujral from '../AlumniCard/AlumniPhotos/RavnoorGujral.png';
 
 
 const Program = () => {
@@ -16,7 +12,7 @@ const Program = () => {
         <h2 className="cohort-1-header">cohort 1</h2>
         <CardRow/>
       </div>
-      <Loader type="line-scale-pulse-out"/>
+      <Loader type="line-scale-pulse-out" />
     </>
   );
 };
