ddecc2a4cb6b0079acbf3992c23047e40f399a62
nstone213 <nbaronstone@gmail.com>
2024-08-12 01:00:33 -0400

    Initial page formatting

--- react-portfolio/src/components/Program/index.js
+++ react-portfolio/src/components/Program/index.js
@@ -5,7 +5,9 @@ import { Card } from '../AlumniCard/Card';
 const Program = () => {
   return (
     <div className="program-page">
-      <Card />
+      <h1 className="program-alumni-header">program alumni</h1>
+      <h2 className="cohort-1-header">cohort 1</h2>
+      
     </div>
   );
 };
