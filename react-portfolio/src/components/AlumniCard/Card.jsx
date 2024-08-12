a4857318907673a2bd8a3cdcc8589fa1f2af3e1f
nstone213 <nbaronstone@gmail.com>
2024-08-12 01:16:06 -0400

    Developed Card component prop

--- react-portfolio/src/components/AlumniCard/Card.jsx
+++ react-portfolio/src/components/AlumniCard/Card.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import './Card.css';
-import Nicholas_Stone from './AlumniPhotos/Nicholas_Stone_image.jpeg';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Loader from 'react-loaders'
 
-
-export const Card = () => {
+export const Card = ({ image, name, description, linkedinUrl }) => {
     return (
         <>
             <div className="card-total">
                 <div className="card-container">
-                    <img src={Nicholas_Stone} alt="Card Image" className="card-image" draggable="false"/>
+                    <img src={image} alt={`${name} Image`} className="card-image" draggable="false"/>
                     <h1 className="card-title">
-                        <a href="https://www.linkedin.com/in/sidbalaga/" target="_blank" rel="noreferrer">
-                            Nicholas Stone
+                        <a href={linkedinUrl} target="_blank" rel="noreferrer">
+                            {name}
                         </a>
                     </h1>
-                    <p className="card-description">Georgia Institute of Technology</p>
+                    <p className="card-description">{description}</p>
                 </div>
             </div>
             <Loader type="line-scale-pulse-out"/>
--- react-portfolio/src/components/Program/index.js
+++ react-portfolio/src/components/Program/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './index.scss';
 import { Card } from '../AlumniCard/Card';
+import Loader from 'react-loaders'
+import Nicholas_Stone from '/Users/nbaronstone/Documents/GitHub/careerhaus-website/react-portfolio/src/components/AlumniCard/AlumniPhotos/Nicholas_Stone_image.jpeg';
 
 const Program = () => {
   return (
-    <div className="program-page">
-      <h1 className="program-alumni-header">program alumni</h1>
-      <h2 className="cohort-1-header">cohort 1</h2>
-      
-    </div>
+   <>
+     <div className="program-page">
+        <h1 className="program-alumni-header">program alumni</h1>
+        <h2 className="cohort-1-header">cohort 1</h2>
+        <Card 
+          image={Nicholas_Stone} 
+          name="Nicholas Stone" 
+          description="Georgia Institute of Technology" 
+          linkedinUrl="https://www.linkedin.com/in/nicholasbaronstone/" 
+        />
+      </div>
+      <Loader type="line-scale-pulse-out"/>
+    </>
   );
 };
 
