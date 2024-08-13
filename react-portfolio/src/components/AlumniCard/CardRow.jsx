2bee7b1618db399430dea710601f446cd29fab2a
nstone213 <nbaronstone@gmail.com>
2024-08-12 22:52:15 -0400

    Cohort 1 fully added

--- react-portfolio/src/components/AlumniCard/CardRow.jsx
+++ react-portfolio/src/components/AlumniCard/CardRow.jsx
@@ -1,50 +1,20 @@
 import React from 'react';
 import './CardRow.css';
 import { Card } from '../AlumniCard/Card';
-import Nicholas_Stone from '../AlumniCard/AlumniPhotos/Nicholas_Stone_image.jpeg';
-import Aabha_Muley from '../AlumniCard/AlumniPhotos/AabhaMuley.png';
-import Ravnoor_Gujral from '../AlumniCard/AlumniPhotos/RavnoorGujral.png';
-import Lakshana_Ramanan from '../AlumniCard/AlumniPhotos/LakshanaRamanan.png';
-import Rohan_Srivatsa from '../AlumniCard/AlumniPhotos/RohanSrivatsa.png';
 
-export const CardRow = () => {
+export const CardRow = ({ cards }) => {
     return (
-            <div className="cardrows1">
-                <div className="card11">
+        <div className="cardrows1">
+            {cards.map((card, index) => (
+                <div key={index} className={`card${index + 1}`}>
                     <Card
-                        image={Aabha_Muley} 
-                        name="Aabha Muley" 
-                        description="University of Georgia" 
-                        linkedinUrl="https://www.linkedin.com/in/aabha-muley/"
+                        image={card.image} 
+                        name={card.name} 
+                        description={card.description} 
+                        linkedinUrl={card.linkedinUrl}
                     />
                 </div>
-
-                <div className="card12">
-                    <Card
-                        image={Ravnoor_Gujral} 
-                        name="Ravnoor Gujral" 
-                        description="Georgia Institute of Technology" 
-                        linkedinUrl="https://www.linkedin.com/in/ravnoorgujral/" 
-                    />
-                </div>
-
-                <div className="card13">
-                    <Card
-                        image={Lakshana_Ramanan} 
-                        name="Lakshana Ramanan" 
-                        description="Georgia Institute of Technology" 
-                        linkedinUrl="https://www.linkedin.com/in/lakshana-ramanan/" 
-                    />
-                </div>
-
-                <div className="card14">
-                    <Card
-                        image={Rohan_Srivatsa} 
-                        name="Rohan Srivatsa" 
-                        description="University of Georgia" 
-                        linkedinUrl="https://www.linkedin.com/in/rohansrivatsa/" 
-                    />
-                </div>
-            </div>
+            ))}
+        </div>
     );
 };
--- react-portfolio/src/components/Program/index.js
+++ react-portfolio/src/components/Program/index.js
@@ -1,20 +1,96 @@
 import React from 'react';
 import './index.scss';
-import Loader from 'react-loaders'
 import { CardRow } from '../AlumniCard/CardRow';
+import Nicholas_Stone from '../AlumniCard/AlumniPhotos/Nicholas_Stone_image.jpeg';
+import Aabha_Muley from '../AlumniCard/AlumniPhotos/AabhaMuley.png';
+import Ravnoor_Gujral from '../AlumniCard/AlumniPhotos/RavnoorGujral.png';
+import Lakshana_Ramanan from '../AlumniCard/AlumniPhotos/LakshanaRamanan.png';
+import Rohan_Srivatsa from '../AlumniCard/AlumniPhotos/RohanSrivatsa.png';
+import Raaga_Sarraff from '../AlumniCard/AlumniPhotos/Raaga_Sarraff.png';
+import Stanley_Yin from '../AlumniCard/AlumniPhotos/StanleyYin.png';
+import Neha_Gurram from '../AlumniCard/AlumniPhotos/NehaGurram.png';
+import Jonas_Freck from '../AlumniCard/AlumniPhotos/JonasFreck.png';
+import Aditi_Satghare from '../AlumniCard/AlumniPhotos/AditiSatghare.png';
 
 
 const Program = () => {
-  return (
-   <>
-     <div className="program-page">
-        <h1 className="program-alumni-header">program alumni</h1>
-        <h2 className="cohort-1-header">cohort 1</h2>
-        <CardRow/>
-      </div>
-      <Loader type="line-scale-pulse-out" />
-    </>
-  );
+    const cohort1Cards = [
+        {
+            image: Aabha_Muley,
+            name: "Aabha Muley",
+            description: "University of Georgia",
+            linkedinUrl: "https://www.linkedin.com/in/aabha-muley/"
+        },
+        {
+            image: Ravnoor_Gujral,
+            name: "Ravnoor Gujral",
+            description: "Georgia Institute of Technology",
+            linkedinUrl: "https://www.linkedin.com/in/ravnoorgujral/"
+        },
+        {
+            image: Lakshana_Ramanan,
+            name: "Lakshana Ramanan",
+            description: "Georgia Institute of Technology",
+            linkedinUrl: "https://www.linkedin.com/in/lakshana-ramanan/"
+        },
+        {
+            image: Rohan_Srivatsa,
+            name: "Rohan Srivatsa",
+            description: "University of Georgia",
+            linkedinUrl: "https://www.linkedin.com/in/rohansrivatsa/"
+        },
+    ];
+
+    const cohort2Cards = [
+      {
+          image: Raaga_Sarraff,
+          name: "Raaga Sarraff",
+          description: "Northview High School",
+          linkedinUrl: "http://www.linkedin.com/in/raaga-sarraff-300a97242"
+      },
+      {
+          image: Stanley_Yin,
+          name: "Stanley Yin",
+          description: "Carnegie Mellon University",
+          linkedinUrl: "http://www.linkedin.com/in/stanleyyin/"
+      },
+      {
+          image: Neha_Gurram,
+          name: "Neha Gurram",
+          description: "Northview High School",
+          linkedinUrl: "http://www.linkedin.com/in/neha-gurram-0818b4258"
+      },
+      {
+          image: Jonas_Freck,
+          name: "Jonas Freck",
+          description: "Kennesaw State University",
+          linkedinUrl: "https://www.linkedin.com/in/jonas-freck/"
+      },
+  ];
+  const cohort3Cards = [
+    {
+        image: Aditi_Satghare,
+        name: "Aditi Satghare",
+        description: "Innovation Academy",
+        linkedinUrl: "https://www.linkedin.com/in/aditisatghare"
+    },
+];
+
+    return (
+        <>
+            <div className="program-page">
+                <h1 className="program-alumni-header">program alumni</h1>
+                <h2 className="cohort-1-header">cohort 1</h2>
+                <CardRow cards={cohort1Cards} />
+                <div className="second">
+                  <CardRow cards={cohort2Cards} />
+                </div>
+                <div className="third">
+                <CardRow cards={cohort3Cards} />
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Program;
